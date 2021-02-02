6946c9aca2df80d5a89c17145ee60067388fc3c8
Anastasiya-Zhyrkevich <Anastasiya.Zhyrkevich@yandex.ru>
2021-02-02 15:25:50 +0300

    ful solution

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+Links: 
+https://developer.okta.com/blog/2018/11/15/node-express-typescript
+https://tutorialedge.net/typescript/testing-typescript-api-with-mocha-chai/
+
+
 1. `npm run start`
 2. `npm run test`
--- specs/book.ts
+++ specs/book.ts
@@ -1,16 +1,23 @@
-import { server } from '../src/server';
-import * as chai from 'chai';
-import chaiHttp = require('chai-http');
+import { server } from '../src';
 import 'mocha';
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('Hello API Request', () => {
   it('should return response on call', () => {
-    return chai.request(server).get('/hello')
-      .then(res => {
-        chai.expect(res.text).to.eql("how's it going?");
-      })
+    return chai.request(server)
+    .get('/')
+    .then(res => {
+      chai.expect(res.body.message).to.eql("Welcome to our Bookstore!");
+    });
   })
+
+  after( function(done) {
+    server.close();
+    done();
+  });
 })
